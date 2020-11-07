a9d6d5aaa1585b60383f76d6758e3ad022e1f7df
Xlayton <weightequalsmasstimesgravity@gmail.com>
2020-11-07 14:15:38 -0700

    Turns

--- front-end/game.js
+++ front-end/game.js
@@ -10,64 +10,75 @@ var virusImage = new Image(100, 100);
 virusImage.src = './Images/virus.png';
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-canvas.addEventListener("click", function(e) {
+canvas.addEventListener("click", function (e) {
     clicked(e)
 })
 const socket = io(`${location.origin}`, {
     path: '/game',
     query: {
-        roomid: "36c2781e-c653-4a1d-9f46-56c098d16b57"
+        roomid: "458f541e-93c4-4f1f-809a-7051f9654528"
     }
 });
 socket.on("message", data => {
-    let rawBoard = JSON.parse(data);
+    let rawBoard;
+    try {
+        rawBoard = JSON.parse(data);
+    } catch (e) {
+        console.log("dont :)")
+        return
+    }
     roomid = rawBoard.id
     board = rawBoard.board
     player = rawBoard.player
     getBoard()
 });
-socket.on("updateboard", data =>  {
-    let rawBoard = JSON.parse(data);
+socket.on("updateboard", data => {
+    let rawBoard;
+    try {
+        rawBoard = JSON.parse(data);
+    } catch (e) {
+        console.log("dont :)")
+        return
+    }
     roomid = rawBoard.id
     board = rawBoard.board
-    player = rawBoard.player
     getBoard()
 })
+
 function getBoard() {
     var size = (canvas.width / board.length) > (canvas.height / board[0].length) ? (canvas.height / board[0].length) : (canvas.width / board.length);
-     for(let i = 0; i < board.length; i++)
-     {
-         for(let i2 = 0; i2 < board[i].length; i2++)
-         {
-             if(board[i][i2] == "Empty")
-             {
-                ctx.strokeRect((i*size), (i2*size), size, size);
+    for (let i = 0; i < board.length; i++) {
+        for (let i2 = 0; i2 < board[i].length; i2++) {
+            if (board[i][i2] == "Empty") {
+                ctx.strokeRect((i * size), (i2 * size), size, size);
+            } else if (board[i][i2] == "Virus") {
+                ctx.strokeRect((i * size), (i2 * size), size, size);
+                ctx.drawImage(virusImage, (i * size), (i2 * size), size, size);
+            } else if (board[i][i2] == "Wall") {
+                ctx.strokeRect((i * size), (i2 * size), size, size);
+                ctx.drawImage(vaccineImage, (i * size), (i2 * size), size, size);
             }
-            else if(board[i][i2] == "Virus")
-            {
-                ctx.strokeRect((i*size), (i2*size), size, size);
-                ctx.drawImage(virusImage, (i*size), (i2*size), size, size);
-             }
-             else if(board[i][i2] == "Wall")
-             {
-                ctx.strokeRect((i*size), (i2*size), size, size);
-                ctx.drawImage(vaccineImage, (i*size), (i2*size), size, size);
-             }
-         }         
-     }
+        }
+    }
 }
+
 function clicked(event) {
     mouseX = event.clientX;
     mouseY = event.clientY;
     var size = (canvas.width / board.length) > (canvas.height / board[0].length) ? (canvas.height / board[0].length) : (canvas.width / board.length);
-    var x = Math.floor((mouseX/size))
-    var y = Math.floor((mouseY/size))
-    if(player == 0)
-    {
-        socket.emit("movevirus", { x : x, y : y, roomid : roomid})
-    }
-    else if(player == 1)
-    {
-        socket.emit("placewall", {x : x, y : y, roomid : roomid})
+    var x = Math.floor((mouseX / size))
+    var y = Math.floor((mouseY / size))
+    if (player == 0) {
+        socket.emit("movevirus", {
+            x: x,
+            y: y,
+            roomid: roomid
+        })
+    } else if (player == 1) {
+        socket.emit("placewall", {
+            x: x,
+            y: y,
+            roomid: roomid
+        })
     }
 }
