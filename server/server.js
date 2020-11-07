99876a3e635a46adc4c4033a5ff73e74a4a53116
Xlayton <weightequalsmasstimesgravity@gmail.com>
2020-11-07 10:06:12 -0700

    Lobby creation, and the real connect code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const uuid = require("uuid")
 const express = require("express")
+const bparser = require("body-parser")
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server)
@@ -26,16 +27,45 @@ const generateBoard = (width, height) => {
 let lobbies = []
 
 app.use(express.static(`${__dirname}/../front-end`))
+app.use(bparser.json({
+    extended: true
+}))
 
-io.on("connect", (sock) => {
-    console.log("Client Connected")
+app.post("/createroom", (req, res) => {
+    let {
+        name,
+        boardwidth,
+        boardheight
+    } = req.body
     let room = {
         id: uuid.v4(),
+        name: name,
         isOpen: true,
-        board: generateBoard(BOARD_HEIGHT, BOARD_HEIGHT)
+        isStarted: false,
+        players: 0,
+        board: generateBoard(boardwidth, boardheight)
     }
     lobbies.push(room)
-    sock.send(room)
+    res.send(JSON.stringify(lobbies))
+})
+
+io.on("connect", (sock) => {
+    console.log("Client Attemping Connection...")
+    let roomid = sock.handshake.query.roomid
+    let lobby = lobbies.filter((lobby) => lobby.id === roomid)[0]
+    if (!lobby || lobby.players >= 2) {
+        console.log(`Client send invalid room id: ${roomid}`)
+        console.log(`All Lobbies: ${lobbies.map(lobby => lobby.id)}`)
+        sock.close()
+        return
+    }
+    lobby.players = lobby.players + 1
+    if (lobby.players >= 2) {
+        lobby.isOpen = false;
+        lobby.isStarted = true;
+    }
+    sock.join(`${lobby.id}`, () => console.log(`Client rooms: ${Object.keys(sock.rooms)}`))
+    sock.send(JSON.stringify(lobby))
 })
 
 server.listen(3000, () => {
