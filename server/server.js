67ff05a6b60b224b3fc2dd54be860a9f9fbd4688
Xlayton <weightequalsmasstimesgravity@gmail.com>
2020-11-07 09:10:23 -0700

    Server setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-const server = require("http").createServer();
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server)
+
+app.use(express.static(`${__dirname}/../front-end`))
+
+io.on("connection", () => {
+    console.log("Client Connected")
+})
+
+app.listen(3000, () => {
+    console.log(`Server running on port ${server.address().port}`)
+})
