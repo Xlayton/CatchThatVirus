616fd81f60aac5e738d63557ddabed9c21aa1748
unknown <zwslocum@yahoo.com>
2020-11-07 13:05:35 -0700

    add canvas stuff

--- front-end/game.js
+++ front-end/game.js
@@ -1,23 +1,54 @@
-const canvas = document.getElementById("gameCanvas")
-const ctx = canvas.getContext("2d")
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+var turn;
+var board;
+var virusImage = new Image(100, 100);
+virusImage.src = './Images/virus.png';
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.addEventListener("click", function(e) {
+    clicked(e)
+})
 const socket = io(`${location.origin}`, {
     path: '/game',
 });
 socket.on("message", data => {
-    console.log(data)
-    console.log("CORS sucks ass")
+    board = data.board;
+    getBoard()
 });
 
 function getBoard() {
-    var myImage = new Image();
-    myImage.src = './Images/cat.png';
-    ctx.drawImage(myImage, 0, 0, 150, 180);
-    // var board = [15][15]
-    // var i
-    // for(i = 0; i < board.length; i++)
-    // {
-    //     board[i].forEach(element => {
-    //         canvas.drawImage("./Images/cat.png", 0 , 0)
-    //     })
-    // }
+    var grassImage = new Image(100, 100);
+    grassImage.src = './Images/index.jpg';
+    var size = (canvas.width / board.length) > (canvas.height / board[0].length) ? (canvas.height / board[0].length) : (canvas.width / board.length);
+     for(let i = 0; i < board.length; i++)
+     {
+         for(let i2 = 0; i2 < board[i].length; i2++)
+         {
+             if(board[i][i2] == "Empty")
+             {
+                ctx.strokeRect((i*size), (i2*size), size, size);
+            }
+            else if(board[i][i2] == "Virus")
+            {
+                ctx.drawImage(virusImage, (i*size), (i2*size), size, size);
+             }
+         }         
+     }
+}
+function clicked(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    var size = (canvas.width / board.length) > (canvas.height / board[0].length) ? (canvas.height / board[0].length) : (canvas.width / board.length);
+    var x = Math.floor((mouseX/size))
+    var y = Math.floor((mouseY/size))
+    console.log(x)
+    console.log(y)
+    console.log(board[x][y])
+    if(board[x][y] == "Empty")
+    {
+        console.log("hello")
+        board[x][y] == "Pillar"
+        ctx.drawImage(virusImage, (x*size), (y*size), size, size);
+    }
 }
